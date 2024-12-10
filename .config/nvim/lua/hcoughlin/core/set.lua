4cf3ae18a3888341610490f3ecb53d6e1aa77b6d
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2024-12-10 14:32:33 -0500

    CPP block comment autocommand

--- .config/nvim/lua/hcoughlin/core/set.lua
+++ .config/nvim/lua/hcoughlin/core/set.lua
@@ -45,84 +45,49 @@ vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
 
 --vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, { desc = "opens split w/ locations for diagnostic messages" })
 
--- Create an autocmd for C/C++ files
--- vim.api.nvim_create_autocmd("FileType", {
---     pattern = { "c", "cpp" },
---     callback = function()
---         -- Set the key mapping for block comment
---         vim.keymap.set('n', '<leader>bc', function()
---             vim.api.nvim_put({ '/**', ' * ', ' */' }, 'l', true, true)
---             vim.cmd('normal! kA')
---         end, { buffer = true, noremap = true, silent = true })
---     end,
--- })
--- Create an autocmd for C/C++ files
--- vim.api.nvim_create_autocmd("FileType", {
---     pattern = { "c", "cpp" },
---     callback = function()
---         -- Set the key mapping for block comment
---         vim.keymap.set('n', '<leader>bc', function()
---             -- Save current cursor position
---             local cursor_pos = vim.api.nvim_win_get_cursor(0)
---             -- Insert the block comment lines
---             vim.api.nvim_put({ '/**', ' * ', ' */' }, 'l', true, true)
---             -- Restore the cursor to the second line (' * ') and move to the end of the line in insert mode
---             vim.api.nvim_win_set_cursor(0, {cursor_pos[1] + 1, 3}) -- Set cursor to ' * ' line, at the end of ' * '
---             vim.cmd('startinsert!') -- Enter insert mode
---         end, { buffer = true, noremap = true, silent = true })
---     end,
--- })
--- Create an autocmd for C/C++ files
--- vim.api.nvim_create_autocmd("FileType", {
---     pattern = { "c", "cpp" },
---     callback = function()
---         -- Set the key mapping for block comment
---         vim.keymap.set('n', '<leader>bc', function()
---             -- Insert the block comment lines
---             vim.api.nvim_put({ '/**', ' * ', ' */' }, 'l', true, true)
---             -- Move the cursor to the middle line (2nd line) after the ' * ' and enter insert mode
---             local current_row = vim.api.nvim_win_get_cursor(0)[1] -- Get current row
---             vim.api.nvim_win_set_cursor(0, {current_row - 1, 3})  -- Move to the 2nd line, after '* '
---             vim.cmd('startinsert!') -- Enter insert mode
---         end, { buffer = true, noremap = true, silent = true })
---     end,
--- })
--- Create an autocmd for C/C++ files
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "c", "cpp" },
     callback = function()
-        -- Set the key mapping for block comment
         vim.keymap.set('n', '<leader>bc', function()
-            -- Get the current row where the cursor is located before inserting the comment
             local current_row = vim.api.nvim_win_get_cursor(0)[1]
+            local shiftwidth = vim.fn.shiftwidth()
 
-            -- Function to find the indentation of the nearest previous indented line
-            local function get_nearest_indent()
-                for i = current_row - 1, 1, -1 do
-                    local line_indent = vim.fn.indent(i)
-                    if line_indent > 0 then
-                        return line_indent
+            local function get_context_indent()
+                local current_line = vim.api.nvim_buf_get_lines(0, current_row-1, current_row, true)[1]
+                local base_indent = vim.fn.indent(current_row)
+
+                -- If line ends with opening brace, increase indent
+                if current_line:match("{%s*$") then
+                    return base_indent + shiftwidth
+                end
+
+                -- Look for parent scope's indent
+                local scope_level = 0
+                for i = current_row, 1, -1 do
+                    local line = vim.api.nvim_buf_get_lines(0, i-1, i, true)[1]
+                    for _ in line:gmatch("}") do
+                        scope_level = scope_level - 1
+                    end
+                    for _ in line:gmatch("{") do
+                        scope_level = scope_level + 1
+                        if scope_level > 0 then
+                            return vim.fn.indent(i) + shiftwidth
+                        end
                     end
                 end
-                return 0 -- Fallback to no indentation if no previous indented line is found
+                return base_indent
             end
 
-            -- Get the nearest indentation level
-            local indent = get_nearest_indent()
-
-            -- Prepare the block comment lines with indentation
+            local indent = get_context_indent()
             local comment_lines = {
                 string.rep(" ", indent) .. "/**",
                 string.rep(" ", indent) .. " * ",
-                string.rep(" ", indent) .. " */",
+                string.rep(" ", indent) .. " */"
             }
 
-            -- Insert the block comment lines
-            vim.api.nvim_put(comment_lines, 'l', true, true)
-
-            -- After inserting, move the cursor to the second line (the ' * ' line)
-            vim.api.nvim_win_set_cursor(0, {current_row + 2, indent + 3})  -- Move to the second line after ' * '
-            vim.cmd('startinsert!') -- Enter insert mode
+            vim.api.nvim_buf_set_lines(0, current_row, current_row, true, comment_lines)
+            vim.api.nvim_win_set_cursor(0, {current_row + 2, indent + 3})
+            vim.cmd('startinsert!')
         end, { buffer = true, noremap = true, silent = true })
     end,
 })
